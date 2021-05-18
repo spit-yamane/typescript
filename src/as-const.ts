c3a3198bddc121d91856d862c4c43f4825413be1
Tomonori_Yamanae <t-yamne@spit.co.jp>
2021-05-19 06:48:38 +0900

    as-const

---
+++ src/as-const.ts
@@ -0,0 +1,16 @@
+export {};
+
+let name = 'Tomonori';
+
+name = 'Yamane';
+
+let nickname = 'Nu' as const;
+// nickname = 'Yamane';
+
+let profile = {
+  name: 'Tomonori',
+  heigth: 160,
+} as const;
+
+// profile.name = 'Yamane';
+// profile.height = 170;
